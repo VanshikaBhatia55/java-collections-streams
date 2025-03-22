faf13b5cb83c42fee2206b458eb841b3acb99791
vanshika <vanshika2260@chitkara.edu.in>
2025-03-22 09:43:41 +0530

    [vanshika] Add. Stack using queues.

--- collections/queue_interface/StackUsingQueues.java
+++ collections/queue_interface/StackUsingQueues.java
@@ -1,2 +1,44 @@
-package java_collections_streams.collections.queue_interface;public class StackUsingQueues {
+package java_collections_streams.collections.queue_interface;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class StackUsingQueues {
+    private Queue<Integer> q1 = new LinkedList<>();
+    private Queue<Integer> q2 = new LinkedList<>();
+
+    public void push(int value) {
+        q2.offer(value);
+        while (!q1.isEmpty()) {
+            q2.offer(q1.poll());
+        }
+        Queue<Integer> temp = q1;
+        q1 = q2;
+        q2 = temp;
+    }
+
+    public int pop() {
+        if (q1.isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return q1.poll();
+    }
+
+    // Top operation: O(1)
+    public int top() {
+        if (q1.isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return q1.peek();
+    }
+
+    public static void main(String[] args) {
+        StackUsingQueues stack = new StackUsingQueues();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        System.out.println("Top: " + stack.top());
+        System.out.println("Pop: " + stack.pop());
+        System.out.println("Top: " + stack.top());
+    }
 }
