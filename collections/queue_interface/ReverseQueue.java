d40bf082e72c000e08bbbce129a9aa4de74ff685
vanshika <vanshika2260@chitkara.edu.in>
2025-03-22 09:43:12 +0530

    [vanshika] Add. Reverse Queue.

--- collections/queue_interface/ReverseQueue.java
+++ collections/queue_interface/ReverseQueue.java
@@ -1,2 +1,25 @@
-package java_collections_streams.collections.queue_interface;public class ReverseQueue {
+package java_collections_streams.collections.queue_interface;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class ReverseQueue {
+    public static void reverseQueue(Queue<Integer> queue) {
+        if (queue.isEmpty() || queue.size() == 1) {
+            return;
+        }
+        int front = queue.poll();
+        reverseQueue(queue);
+        queue.offer(front);
+    }
+
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+        queue.offer(10);
+        queue.offer(20);
+        queue.offer(30);
+        System.out.println("Original Queue: " + queue);
+        reverseQueue(queue);
+        System.out.println("Reversed Queue: " + queue);
+    }
 }
