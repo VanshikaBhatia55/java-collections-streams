ee24b5bfebad522e615ec35200e1d2a589315b40
vanshika <vanshika2260@chitkara.edu.in>
2025-03-22 09:42:11 +0530

    [vanshika] Add. Circular Buffer.

--- collections/queue_interface/CircularBuffer.java
+++ collections/queue_interface/CircularBuffer.java
@@ -1,2 +1,48 @@
-package java_collections_streams.collections.queue_interface;public class CircularBuffer {
+package java_collections_streams.collections.queue_interface;
+
+public class CircularBuffer {
+    private int[] buffer;
+    private int size;
+    private int front;
+    private int rear;
+    private int count;
+
+    public CircularBuffer(int capacity) {
+        this.size = capacity;
+        this.buffer = new int[size];
+        this.front = 0;
+        this.rear = 0;
+        this.count = 0;
+    }
+
+    public void insert(int value) {
+        buffer[rear] = value;
+        rear = (rear + 1) % size;
+
+        if (count < size) {
+            count++;
+        } else {
+            front = (front + 1) % size;
+        }
+    }
+
+    public void display() {
+        System.out.print("Buffer: [");
+        for (int i = 0; i < count; i++) {
+            int index = (front + i) % size;
+            System.out.print(buffer[index]);
+            if (i < count - 1) System.out.print(", ");
+        }
+        System.out.println("]");
+    }
+
+    public static void main(String[] args) {
+        CircularBuffer cb = new CircularBuffer(3);
+        cb.insert(1);
+        cb.insert(2);
+        cb.insert(3);
+        cb.display();
+        cb.insert(4);
+        cb.display();
+    }
 }
