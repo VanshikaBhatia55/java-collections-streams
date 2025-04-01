5e258367b1c6e7f6afb9f34e8fc52bf08ad25c3a
vanshika <vanshika2260@chitkara.edu.in>
2025-04-01 18:36:25 +0530

    [Vanshika] Add. Database Connection.

---
+++ junit/src/main/java/basic/DatabaseConnection.java
@@ -0,0 +1,17 @@
+package java_collections_streams.junit.src.main.java.basic;
+
+public class DatabaseConnection {
+    private boolean isConnected;
+    public void connect() {
+        isConnected = true;
+        System.out.println("Database connected.");
+    }
+
+    public void disconnect() {
+        isConnected = false;
+        System.out.println("Database disconnected.");
+    }
+    public boolean isConnected() {
+        return isConnected;
+    }
+}
