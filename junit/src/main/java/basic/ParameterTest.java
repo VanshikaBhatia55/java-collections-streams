68cd61abd6cd59f0e8e2c9bd145337081cb95e9a
vanshika <vanshika2260@chitkara.edu.in>
2025-04-01 18:38:18 +0530

    [Vanshika] Add. Parameter Test.

---
+++ junit/src/main/java/basic/ParameterTest.java
@@ -0,0 +1,8 @@
+package java_collections_streams.junit.src.main.java.basic;
+
+public class ParameterTest {
+    public static boolean isEven(int number){
+        if(number%2==0)return true;
+        return false;
+    }
+}
