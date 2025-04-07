cc4237d619b6330ff2a346c7737d45057d08440d
vanshika <vanshika2260@chitkara.edu.in>
2025-04-07 10:38:10 +0530

    [Vanshika] Add. Legacy Api.

---
+++ annotation/Deprecated/Main.java
@@ -0,0 +1,9 @@
+package java_collections_streams.annotation.Deprecated;
+
+public class Main {
+    public static void main(String[] args) {
+        LegacyAPI api = new LegacyAPI();
+        api.oldFeature();
+        api.newFeature();
+    }
+}
