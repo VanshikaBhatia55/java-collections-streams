5e2eb7dcaf594519f78285b82e6cc4c13085c263
vanshika <vanshika2260@chitkara.edu.in>
2025-04-07 10:34:37 +0530

    [Vanshika] Add. Log Execution Time.

---
+++ annotation/custom/log_execution_time/ExecutionTime.java
@@ -0,0 +1,19 @@
+package java_collections_streams.annotation.custom.log_execution_time;
+
+public class ExecutionTime {
+    @LogExecutionTime
+    public void method1(){
+        try {
+            Thread.sleep(500);
+        }
+        catch (InterruptedException e){
+            System.out.println(e.getMessage());
+        }
+    }
+    @LogExecutionTime
+    public void method2(){
+        for (int i = 0; i < 1000; i++) {
+
+        }
+    }
+}
